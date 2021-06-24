241a8bf533520ce8fcd92d12cba6764403518061
yumaeda <yumaeda@gmail.com>
2021-06-24 15:10:01 +0900

    Create insert_wine.sql

---
+++ misc/insert_wine.sql
@@ -0,0 +1,13 @@
+INSERT INTO wines (id, cepage, color, comment, country, name, name_jpn, region, region_jpn, vintage)
+     VALUES (
+         UuidToBin(UUID()),
+         'ピノ・グリ100%',
+         1,
+         '厚みのある柑橘果実のアロマと魅惑的なミネラル感が幾層にも広がります。深いコクと酸は食事との相性も抜群です。しっかりボディの白が好きな方に特にオススメ！',
+         'New Zealand',
+         'Marlborough Homecreek Pinot Gris',
+         'マールボロ・ホームクリーク・ピノ・グリ',
+         'South Island',
+         '南島' ,
+         '2018'
+     );
